ddf22576adeee5020322f48896758cd545dce22f
James Narayanan <jamesnarayanan@gmail.com>
2020-06-18 18:15:28 -0400

    Minor Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var contentCol, colCenter;
+var toc, contentCol, colCenter;
 
 $(document).ready(event => {
-	let toc = $("#toc"); // Table of contents
+	toc = $("#toc"); // Table of contents
 	let opac = window.scrollY <= 500 ? window.scrollY / 500 : 100;
 	toc.css({
 		opacity: opac
@@ -9,38 +9,24 @@ $(document).ready(event => {
 
 	contentCol = $("#content-col");
 	colCenter = contentCol.offset().left + contentCol.outerWidth()/2; // Length from left side of screen to center of col
-	if(window.innerWidth >= 1200) { // Window is XL
-		let diff = window.innerWidth/2 - colCenter;
-		let shift = diff - window.scrollY/2 > 0 ? diff - window.scrollY/2 : 0;
-		contentCol.css({
-			left: shift
-		});
-	}
+	slideAndFade();
 });
 
 $(window).resize(event => {
 	colCenter = contentCol.offset().left + contentCol.outerWidth()/2; // Length from left side of screen to center of col
-	if(window.innerWidth >= 1200) {
-		let diff = window.innerWidth/2 - colCenter;
-		let shift = diff - window.scrollY/2 > 0 ? diff - window.scrollY/2 : 0;
-		contentCol.css({
-			left: shift
-		});
-	}
-	else {
-		contentCol.css({
-			left: 0
-		});
-	}
+	slideAndFade();
 });
 
 $(document).scroll(event => {
-	let toc = $("#toc");
 	let opac = window.scrollY <= 500 ? window.scrollY / 500 : 100;
 	toc.css({
 		opacity: opac
 	});
 
+	slideAndFade();
+});
+
+function slideAndFade() {
 	if(window.innerWidth >= 1200) {
 		let diff = window.innerWidth/2 - colCenter;
 		let shift = diff - window.scrollY/2 > 0 ? diff - window.scrollY/2 : 0;
@@ -53,4 +39,4 @@ $(document).scroll(event => {
 			left: 0
 		});
 	}
-});
+}
