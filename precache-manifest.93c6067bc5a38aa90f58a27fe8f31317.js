7dcdbb0095a5afb7744e65420c7a9d5739c22b0b
Victor Hu <victorhu3@gmail.com>
2020-06-24 16:42:28 -0400

    Updates

---
+++ precache-manifest.93c6067bc5a38aa90f58a27fe8f31317.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39150474cbc800c2fd1841bd06ae66bf",
+    "url": "/Team41Robotics/RoboGuide/index.html"
+  },
+  {
+    "revision": "b04b4574104df9fc732f",
+    "url": "/Team41Robotics/RoboGuide/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b972493834f5d54a5c43",
+    "url": "/Team41Robotics/RoboGuide/static/css/main.e0552eee.chunk.css"
+  },
+  {
+    "revision": "b04b4574104df9fc732f",
+    "url": "/Team41Robotics/RoboGuide/static/js/2.0f75b5f7.chunk.js"
+  },
+  {
+    "revision": "6f5cd516e72ccce36acd07f8745d4216",
+    "url": "/Team41Robotics/RoboGuide/static/js/2.0f75b5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b972493834f5d54a5c43",
+    "url": "/Team41Robotics/RoboGuide/static/js/main.13a2b619.chunk.js"
+  },
+  {
+    "revision": "c72f7b022c3ea5087a09",
+    "url": "/Team41Robotics/RoboGuide/static/js/runtime-main.12e4f132.js"
+  },
+  {
+    "revision": "8884170706946249da7026482083c9cf",
+    "url": "/Team41Robotics/RoboGuide/static/media/DriverStationUSB.88841707.png"
+  },
+  {
+    "revision": "7c4765afd4705952a795e3209699a670",
+    "url": "/Team41Robotics/RoboGuide/static/media/blur.7c4765af.jpg"
+  },
+  {
+    "revision": "f1f9060b6bc8ec371ab3e325d74f5e7f",
+    "url": "/Team41Robotics/RoboGuide/static/media/functionEx1.f1f9060b.svg"
+  },
+  {
+    "revision": "3795300209e7d093bcaaaa9966038d53",
+    "url": "/Team41Robotics/RoboGuide/static/media/functionEx2.37953002.svg"
+  },
+  {
+    "revision": "7afcbc360d112f12b770c04276fecd78",
+    "url": "/Team41Robotics/RoboGuide/static/media/functionEx2Call.7afcbc36.svg"
+  },
+  {
+    "revision": "8885ce9860f3aed4c999dc1b9fb70633",
+    "url": "/Team41Robotics/RoboGuide/static/media/javaLogo.8885ce98.png"
+  },
+  {
+    "revision": "559ef113ab06995b5ef6d17506878321",
+    "url": "/Team41Robotics/RoboGuide/static/media/joystick.559ef113.jpg"
+  },
+  {
+    "revision": "6506525947ebbf4ca0a12d443a31511b",
+    "url": "/Team41Robotics/RoboGuide/static/media/print.65065259.svg"
+  },
+  {
+    "revision": "669be8987a4e50d8fc5e3ac23424571f",
+    "url": "/Team41Robotics/RoboGuide/static/media/servo.669be898.jpg"
+  },
+  {
+    "revision": "b7b7cbaaddd3eacaf1ba5133c35fa333",
+    "url": "/Team41Robotics/RoboGuide/static/media/sparkMax.b7b7cbaa.png"
+  },
+  {
+    "revision": "ee5a0797664384a78de24b9f0782348d",
+    "url": "/Team41Robotics/RoboGuide/static/media/talonFX.ee5a0797.png"
+  },
+  {
+    "revision": "1a22e0c882b4f59e9b35fc9972c69644",
+    "url": "/Team41Robotics/RoboGuide/static/media/talonSRX.1a22e0c8.jpg"
+  }
+]);
