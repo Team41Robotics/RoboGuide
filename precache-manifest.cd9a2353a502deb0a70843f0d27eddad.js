7ca442c42bcd02eb9de7f1d69283fce29cd38d29
Peter Linder <palinder@umich.edu>
2020-06-25 13:02:17 -0400

    Updates

---
+++ precache-manifest.cd9a2353a502deb0a70843f0d27eddad.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661f59bd51e7e91f8e45add0ef5edf10",
+    "url": "/RoboGuide/index.html"
+  },
+  {
+    "revision": "b7ffafbcc2202509fc63",
+    "url": "/RoboGuide/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4563c812e36ebda943c3",
+    "url": "/RoboGuide/static/css/main.21f6fac8.chunk.css"
+  },
+  {
+    "revision": "b7ffafbcc2202509fc63",
+    "url": "/RoboGuide/static/js/2.7a10e745.chunk.js"
+  },
+  {
+    "revision": "6f5cd516e72ccce36acd07f8745d4216",
+    "url": "/RoboGuide/static/js/2.7a10e745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4563c812e36ebda943c3",
+    "url": "/RoboGuide/static/js/main.a1c90faf.chunk.js"
+  },
+  {
+    "revision": "0054a96a947335541662",
+    "url": "/RoboGuide/static/js/runtime-main.c477eab6.js"
+  },
+  {
+    "revision": "8884170706946249da7026482083c9cf",
+    "url": "/RoboGuide/static/media/DriverStationUSB.88841707.png"
+  },
+  {
+    "revision": "7c4765afd4705952a795e3209699a670",
+    "url": "/RoboGuide/static/media/blur.7c4765af.jpg"
+  },
+  {
+    "revision": "ae0ec440ede9f7f99e8847910954a93c",
+    "url": "/RoboGuide/static/media/frc_programming.ae0ec440.jpg"
+  },
+  {
+    "revision": "f1f9060b6bc8ec371ab3e325d74f5e7f",
+    "url": "/RoboGuide/static/media/functionEx1.f1f9060b.svg"
+  },
+  {
+    "revision": "3795300209e7d093bcaaaa9966038d53",
+    "url": "/RoboGuide/static/media/functionEx2.37953002.svg"
+  },
+  {
+    "revision": "7afcbc360d112f12b770c04276fecd78",
+    "url": "/RoboGuide/static/media/functionEx2Call.7afcbc36.svg"
+  },
+  {
+    "revision": "8885ce9860f3aed4c999dc1b9fb70633",
+    "url": "/RoboGuide/static/media/javaLogo.8885ce98.png"
+  },
+  {
+    "revision": "559ef113ab06995b5ef6d17506878321",
+    "url": "/RoboGuide/static/media/joystick.559ef113.jpg"
+  },
+  {
+    "revision": "38ee63894ab7556bdd4db8006fa82f31",
+    "url": "/RoboGuide/static/media/logo.38ee6389.png"
+  },
+  {
+    "revision": "6506525947ebbf4ca0a12d443a31511b",
+    "url": "/RoboGuide/static/media/print.65065259.svg"
+  },
+  {
+    "revision": "669be8987a4e50d8fc5e3ac23424571f",
+    "url": "/RoboGuide/static/media/servo.669be898.jpg"
+  },
+  {
+    "revision": "b7b7cbaaddd3eacaf1ba5133c35fa333",
+    "url": "/RoboGuide/static/media/sparkMax.b7b7cbaa.png"
+  },
+  {
+    "revision": "ee5a0797664384a78de24b9f0782348d",
+    "url": "/RoboGuide/static/media/talonFX.ee5a0797.png"
+  },
+  {
+    "revision": "1a22e0c882b4f59e9b35fc9972c69644",
+    "url": "/RoboGuide/static/media/talonSRX.1a22e0c8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a55a53a0f5baff44de1f9dc9fc74b0d7.js"
+  "/RoboGuide/precache-manifest.cd9a2353a502deb0a70843f0d27eddad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboGuide/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
