858bfb3c94ba73b79c70cdd994822b15da5be25a
Peter Linder <palinder@umich.edu>
2020-06-25 00:07:10 -0400

    Full navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import logo from "../img/logo.png";
+
 import "../css/NavBar.css";
 
 const navPages = [
@@ -14,24 +16,48 @@ export default function(props) {
   const activePath = loc.pathname + (loc.pathname.endsWith("/") ? "" : "/");
 
   return (
-    <nav className="mb-3">
-      <div
-        className="nav nav-pills nav-fill justify-content-center"
-        id="nav-tab"
-        role="tablist"
+    <nav className="navbar navbar-expand-lg navbar-dark" id="navbar">
+      <Link className="navbar-brand" to="/">
+        <img
+          src={logo}
+          className="d-inline-block align-top"
+          alt=""
+          loading="lazy"
+          style={{
+            width: "30px",
+            height: "30px",
+            marginRight: "10px",
+            marginLeft: "10px"
+          }}
+        />
+        RoboGuide
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        {navPages.map(page => (
-          <Link
-            className={
-              "nav-item nav-link text-white" +
-              (page.path === activePath ? " active" : "")
-            }
-            key={page.path}
-            to={page.path}
-          >
-            {page.title}
-          </Link>
-        ))}
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto">
+          {navPages.map(page => (
+            <li
+              key={page.path}
+              className={
+                "nav-item" + (page.path === activePath ? " active" : "")
+              }
+            >
+              <Link className="nav-link" to={page.path}>
+                {page.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
