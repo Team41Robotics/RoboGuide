010a2e3ad8a046cf0abd92afc56f9aa1e63ddcef
Peter Linder <palinder@umich.edu>
2020-06-26 18:00:33 -0400

    Disable smooth scroll between pages

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import $ from "jquery";
 
 export default function ScrollToTop() {
 	const { pathname } = useLocation();
 
 	useEffect(() => {
+		$("html").css("scroll-behavior", "auto");
 		window.scrollTo(0, 0);
+		$("html").css("scroll-behavior", "smooth");
 	}, [pathname]);
 
 	return null;
