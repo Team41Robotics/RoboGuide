d6c73cc75b4171db9da015bac409178a401f21e3
James Narayanan <jamesnarayanan@gmail.com>
2020-06-26 14:09:41 -0400

    Update not found page (#17)

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+import teek from "../img/sadTeek.png";
+
 export default function(props) {
 	return (
 		<>
 			<Helmet>
 				<title>Not Found</title>
 			</Helmet>
-			<h1>Not Found</h1>
+			<div className="d-flex flex-column align-items-center">
+					<h1>Uh Oh! Page not found :(</h1>
+					<img src={teek} width="300px"/>
+					<h1><a href="/" target="_self">Go Home</a></h1>
+			</div>
 		</>
 	);
 }
