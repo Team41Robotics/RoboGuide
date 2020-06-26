f57bfbbaeda9c16e30ca988cbedd7a815c4c907e
Peter Linder <palinder@umich.edu>
2020-06-26 15:18:16 -0400

    Extremely styled not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,18 +1,83 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 import teek from "../img/sadTeek.png";
 
+const useMousePosition = () => {
+	const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+	const updateMousePosition = ev => {
+		setMousePosition({ x: ev.clientX, y: ev.clientY });
+	};
+
+	useEffect(() => {
+		window.addEventListener("mousemove", updateMousePosition);
+
+		return () => window.removeEventListener("mousemove", updateMousePosition);
+	}, []);
+
+	return mousePosition;
+};
+
 export default function(props) {
+	const ref = useRef();
+	const offset = ref.current
+		? {
+				left: ref.current.offsetLeft,
+				top: ref.current.offsetTop,
+				width: ref.current.offsetWidth,
+				height: ref.current.offsetHeight
+		  }
+		: { left: 0, top: 0, width: 1, height: 1 };
+
+	const mouse = useMousePosition();
+
+	const centerX = offset.left + offset.width / 2;
+	const centerY = offset.top + offset.height / 2;
+	const x = mouse.x - centerX;
+	const y = mouse.y - centerY;
+	const angleX = -y / offset.height / 2;
+	const angleY = x / offset.width / 2;
+	const maxAngle = 30;
+	const rotateX =
+		Math.sign(angleX) * Math.min(Math.abs(angleX), maxAngle).toFixed(2);
+	const rotateY =
+		Math.sign(angleY) * Math.min(Math.abs(angleY), maxAngle).toFixed(2);
+
+	const imgStyle = {
+		transform: "rotateX(" + rotateX + "deg) rotateY(" + rotateY + "deg)",
+		boxShadow: "2px 2px 50px rgba(0, 0, 0, 0.9)",
+		margin: "40px"
+	};
+	const headerStyle = {
+		marginLeft: -100 * rotateY + "px"
+	};
+	const footerStyle = {
+		marginRight: -100 * rotateY + "px"
+	};
+
 	return (
 		<>
 			<Helmet>
 				<title>Not Found</title>
 			</Helmet>
-			<div className="d-flex flex-column align-items-center">
-					<h1>Uh Oh! Page not found :(</h1>
-					<img src={teek} width="300px"/>
-					<h1><a href="/" target="_self">Go Home</a></h1>
+			<div
+				className="d-flex flex-column align-items-center"
+				style={{
+					perspective: "40px"
+				}}
+			>
+				<h1 style={headerStyle}>Uh oh! Page not found!</h1>
+				<img
+					src={teek}
+					width="300px"
+					id="teek404"
+					ref={ref}
+					alt="Sad Prateek"
+					style={imgStyle}
+				/>
+				<h1 style={footerStyle}>You've made Prateek sad :(</h1>
 			</div>
 		</>
 	);
