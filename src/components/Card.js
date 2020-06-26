b757d5ca7217be725ef1418d9eddf7231b3ebe91
Peter Linder <palinder@umich.edu>
2020-06-26 16:39:32 -0400

    Rewrite Card as functional component
    Change from class component to functional because React is moving away from class components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,43 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import { Link } from "react-router-dom";
 
-export default class Card extends React.Component {
-	render() {
-		let footerClass;
-		if(this.props.footerText)
-			footerClass = "card-footer h" + this.props.footerSize;
-		let bannerImgAlt;
-		if(this.props.bannerImg) {
-			let bannerImgWords = this.props.bannerImg.split("/");
-			bannerImgAlt = bannerImgWords[bannerImgWords.length - 1].split(".")[0];
-		}
+export default function Card(props) {
+	const footerText = props.footerText;
+	const footerSize = props.footerSize;
+	const footerLink = props.footerLink;
+	const bannerImg = props.bannerImg;
+	const title = props.title;
+	const children = props.children;
 
-		return (
-			<div className="card">
-				{this.props.bannerImg && <img src={this.props.bannerImg} alt={bannerImgAlt} className="card-img-top"/>}
-				<div className="card-body">
-					{this.props.title && <h4 className="card-title">{this.props.title}</h4>}
-					{this.props.children && <p className="card-text">{this.props.children}</p>}
-				</div>
-				{this.props.footerText &&
-					<div className={footerClass}>
-						{this.props.footerLink ? 
-							<Link to={this.props.footerLink}>{this.props.footerText}</Link>
-							: <>{this.props.footerText}</>
-						}
-					</div>
-				}
-			</div>
-		);
+	let bannerImgAlt;
+	if (bannerImg) {
+		let bannerImgWords = bannerImg.split("/");
+		bannerImgAlt = bannerImgWords[bannerImgWords.length - 1].split(".")[0];
 	}
+
+	return (
+		<div className="card">
+			{bannerImg && (
+				<img src={bannerImg} alt={bannerImgAlt} className="card-img-top" />
+			)}
+			<div className="card-body">
+				{title && <h4 className="card-title">{title}</h4>}
+				{children && <p className="card-text">{children}</p>}
+			</div>
+			{footerText && (
+				<div className={"card-footer h" + footerSize}>
+					{footerLink ? <Link to={footerLink}>{footerText}</Link> : footerText}
+				</div>
+			)}
+		</div>
+	);
 }
 
 Card.propTypes = {
 	bannerImg: PropTypes.string,
 	title: PropTypes.string,
-	bodyText: PropTypes.string,
 	footerText: PropTypes.string,
 	footerLink: PropTypes.string,
 	footerSize: PropTypes.number
