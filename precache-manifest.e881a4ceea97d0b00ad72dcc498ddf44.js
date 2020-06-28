b89632425e0aee892d67d829df20ab3c9e0f1812
James Narayanan <jamesnarayanan@gmail.com>
2020-06-27 20:47:44 -0400

    Updates

--- precache-manifest.e881a4ceea97d0b00ad72dcc498ddf44.js
+++ precache-manifest.e881a4ceea97d0b00ad72dcc498ddf44.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2dbd7fb102932e69c3d7db20e6f5ffbd",
+    "revision": "dc4320bd57afb7150f6241b58443ffac",
     "url": "/index.html"
   },
   {
-    "revision": "16e07394a6d602283994",
+    "revision": "4643b08429509e5bf7b8",
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "343d4952b59ecac3bf6b",
-    "url": "/static/css/main.978c191b.chunk.css"
+    "revision": "a0624a4db44022492b84",
+    "url": "/static/css/main.2027c29b.chunk.css"
   },
   {
-    "revision": "16e07394a6d602283994",
-    "url": "/static/js/2.1f4ce570.chunk.js"
+    "revision": "4643b08429509e5bf7b8",
+    "url": "/static/js/2.f995c79d.chunk.js"
   },
   {
     "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
-    "url": "/static/js/2.1f4ce570.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f995c79d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "343d4952b59ecac3bf6b",
-    "url": "/static/js/main.bf2a0481.chunk.js"
+    "revision": "a0624a4db44022492b84",
+    "url": "/static/js/main.01643c40.chunk.js"
   },
   {
     "revision": "5f9eedae243c6bf05cfa",
@@ -32,20 +32,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/DriverStationUSB.88841707.png"
   },
   {
+    "revision": "bd0d7c9dfd2c6a1941ecd1ce9b8c8d87",
+    "url": "/static/media/electricalBanner.bd0d7c9d.png"
+  },
+  {
     "revision": "7c4765afd4705952a795e3209699a670",
     "url": "/static/media/hardwareBanner.7c4765af.jpg"
   },
   {
-    "revision": "bc6fafac4b00fcddf6b1c3343033cbfd",
-    "url": "/static/media/javaBanner.bc6fafac.svg"
+    "revision": "6bae9eb108e12af9ba80f3d9cb971b9d",
+    "url": "/static/media/javaBanner.6bae9eb1.svg"
   },
   {
     "revision": "559ef113ab06995b5ef6d17506878321",
     "url": "/static/media/joystick.559ef113.jpg"
   },
   {
-    "revision": "38ee63894ab7556bdd4db8006fa82f31",
-    "url": "/static/media/logo.38ee6389.png"
+    "revision": "90bfb14e2266c2ceee04970cf6b772a8",
+    "url": "/static/media/laptopGreen.90bfb14e.svg"
   },
   {
     "revision": "ae0ec440ede9f7f99e8847910954a93c",
@@ -58,6 +62,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "669be8987a4e50d8fc5e3ac23424571f",
     "url": "/static/media/servo.669be898.jpg"
+  },
+  {
+    "revision": "72064540008d041c539ded4951550364",
+    "url": "/static/media/solderingIron.72064540.png"
+  },
+  {
+    "revision": "dd3d6149960150416a456f7acba85241",
+    "url": "/static/media/solderingIronWithHandGreen.dd3d6149.svg"
   },
   {
     "revision": "b7b7cbaaddd3eacaf1ba5133c35fa333",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.020a9bac770e70327cb013308b744659.js"
+  "/precache-manifest.e881a4ceea97d0b00ad72dcc498ddf44.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
